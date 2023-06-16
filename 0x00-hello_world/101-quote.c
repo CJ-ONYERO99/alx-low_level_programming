4d05f434dc8b97ecc195a5bdb00d8e5f9e6a852b
Chimezie Joshua Onyero <chimeziejoshuaonyero999@gmail.com>
2023-06-15 19:01:42 -0700

    All tasks completed

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <unistd.h>
 #include <string.h>
-/*
+/**
 * main - program start
 */
 int main(void)
 {
 	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
 	write(2, msg, strlen(msg));
 	return (1);
 }
