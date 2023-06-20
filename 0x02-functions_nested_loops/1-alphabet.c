481b1ad24c61845670650310b178c458104170c8
Chimezie Joshua Onyero <chimeziejoshuaonyero999@gmail.com>
2023-06-20 15:10:26 -0700

    Task 1 completed

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,32 +1,16 @@
 #include "main.h"
 /**
  * print_alphabet - prints alphabets
- * _putchar - Custom putchar function
- * @c: The character to be printed
- * Return: returns 0 (success)
  */
-int _putchar(char c)
-{
-	return (_putchar(c));
-}
 void print_alphabet(void)
 {
 	char c = 'a';
 
 	while (c <= 'z')
 	{
-		_putchar(c);
-		c++;
+	_putchar(c);
+	c++;
 	}
 
 	_putchar('\n');
-}
-/**
-* main - Entry point
-* Return: returns 0 (success)
-*/
-int main(void)
-{
-	print_alphabet();
-	return (0);
 }
