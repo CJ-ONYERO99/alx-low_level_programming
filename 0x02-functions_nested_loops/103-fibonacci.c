43ed923f0946f07acae719fd93da75c8f1a3a79f
Chimezie Joshua Onyero <chimeziejoshuaonyero999@gmail.com>
2023-06-21 19:49:35 -0700

    Task Done

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the sum of even-valued Fibonacci terms up to 4,000,000
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    unsigned long int fib1 = 1, fib2 = 2, fib_sum = 0, even_sum = 0;
+
+    while (fib1 <= 4000000)
+    {
+        if (fib1 % 2 == 0)
+            even_sum += fib1;
+
+        fib_sum = fib1 + fib2;
+        fib1 = fib2;
+        fib2 = fib_sum;
+    }
+
+    printf("%lu\n", even_sum);
+
+    return 0;
+}
