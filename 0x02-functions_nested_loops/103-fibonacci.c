479fc17eced45e4cec41b62239f159f938469676
Chimezie Joshua Onyero <chimeziejoshuaonyero999@gmail.com>
2023-06-21 19:57:42 -0700

    Done

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,19 +7,19 @@
  */
 int main(void)
 {
-    unsigned long int fib1 = 1, fib2 = 2, fib_sum = 0, even_sum = 0;
+	unsigned long int fib1 = 1, fib2 = 2, fib_sum = 0, even_sum = 0;
 
-    while (fib1 <= 4000000)
-    {
-        if (fib1 % 2 == 0)
-            even_sum += fib1;
+	while (fib1 <= 4000000)
+	{
+	if (fib1 % 2 == 0)
+		even_sum += fib1;
 
-        fib_sum = fib1 + fib2;
-        fib1 = fib2;
-        fib2 = fib_sum;
-    }
+		fib_sum = fib1 + fib2;
+		fib1 = fib2;
+		fib2 = fib_sum;
+	}
 
-    printf("%lu\n", even_sum);
+	printf("%lu\n", even_sum);
 
-    return 0;
+	return (0);
 }
