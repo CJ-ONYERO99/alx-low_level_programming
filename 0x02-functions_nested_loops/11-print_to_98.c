76de1e1d529ae430c4a3dfd695851985f9388e5e
Chimezie Joshua Onyero <chimeziejoshuaonyero999@gmail.com>
2023-06-21 19:26:25 -0700

    Task 11 completed

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 
 /**
- * print_to_98 - Prints all natural numbers from n to 98
+ * print_to_98 - Prints all natural numbers 
+ * from n to 98
  * @n: The starting number
  */
 void print_to_98(int n)
