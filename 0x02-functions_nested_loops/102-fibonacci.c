76adf6bb0b15d05787f681b4d3c462ee2552f632
Chimezie Joshua Onyero <chimeziejoshuaonyero999@gmail.com>
2023-06-21 20:08:00 -0700

    Done

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	unsigned long int prev = 1, curr = 2, next, count;
+
+	printf("%lu, %lu, ", prev, curr);
+
+	for (count = 3; count <= 50; count++)
+	{
+		next = prev + curr;
+		printf("%lu", next);
+
+		if (count < 50)
+			printf(", ");
+		else
+			printf("\n");
+
+		prev = curr;
+		curr = next;
+	}
+
+	return 0;
+}
