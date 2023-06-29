d966b37a4d2401cecfd44e381fded8b086b55898
Chimezie Joshua Onyero <chimeziejoshuaonyero999@gmail.com>
2023-06-29 15:44:21 -0700

    Advanced Task 102 Done

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -5,7 +5,6 @@
  *
  * Return: Always 0
  */
-
 int main(void)
 {
 	int n;
@@ -15,8 +14,18 @@ int main(void)
 	a[2] = 1024;
 	p = &n;
 
-	sprintf((char *)p, "%s", "98");
+	/*
+	 * Write your line of code here...
+	 * Remember:
+	 * - You are not allowed to use 'a'
+	 * - You are not allowed to modify 'p'
+	 * - Only one statement
+	 * - You are not allowed to code anything else than this line of code
+	 */
+	*(p + 5) = 98;
 
 	printf("a[2] = %d\n", a[2]);
+
 	return (0);
 }
+
